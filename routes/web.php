878ee7584e4b0e7925f9f90daac7805891f86f76
daffaabp <daffa.budi2003@gmail.com>
2024-04-03 09:33:55 +0700

    tambah fitur verify email versi 1

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home_new');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home_new')->middleware('verified');
