5247c2ede110a61d8c2173e295ceecb60f669dc3
daffaabp <daffa.budi2003@gmail.com>
2024-04-14 22:03:30 +0700

    ubah migrasi users ke setingan awal

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('username')->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('isPasien')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
